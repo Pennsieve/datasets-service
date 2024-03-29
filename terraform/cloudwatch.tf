6000f1d24f5b88b540082bc376faa5c468f8ac01
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-03-29 15:13:10 -0400

    adding datadog log streaming

---
+++ terraform/cloudwatch.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudwatch_log_group" "datasets_service_lambda_loggroup" {
+  name              = "/aws/lambda/${aws_lambda_function.service_lambda.function_name}"
+  retention_in_days = 30
+  tags = local.common_tags
+}
+resource "aws_cloudwatch_log_subscription_filter" "datasets_service_log_group_subscription" {
+  name            = "${aws_cloudwatch_log_group.datasets_service_lambda_loggroup.name}-subscription"
+  log_group_name  = aws_cloudwatch_log_group.datasets_service_lambda_loggroup.name
+  filter_pattern  = ""
+  destination_arn = data.terraform_remote_state.region.outputs.datadog_delivery_stream_arn
+  role_arn        = data.terraform_remote_state.region.outputs.cw_logs_to_datadog_logs_firehose_role_arn
+}
