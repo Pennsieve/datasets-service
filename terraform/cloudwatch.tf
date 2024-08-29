341e789ee80240bae7eb76b2e83dd7c36b049581
Joost Wagenaar <1605953+jwagenaar@users.noreply.github.com>
2024-08-29 18:16:08 -0400

    add datadog subscription

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_log_group" "manifest_worker_lambda_loggroup" {
   retention_in_days = 30
   tags = local.common_tags
 }
-resource "aws_cloudwatch_log_subscription_filter" "datasets_service_log_group_subscription" {
+resource "aws_cloudwatch_log_subscription_filter" "manifest_worker_log_group_subscription" {
   name            = "${aws_cloudwatch_log_group.manifest_worker_lambda_loggroup.name}-subscription"
   log_group_name  = aws_cloudwatch_log_group.manifest_worker_lambda_loggroup.name
   filter_pattern  = ""
